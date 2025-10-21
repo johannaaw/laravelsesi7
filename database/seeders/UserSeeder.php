e89ec5e9ac7d5f7f401f8b1e82684fcf0c2223d0
johannaaw <johannaangel2907@gmail.com>
2025-10-21 12:17:20 +0700

    one to one

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $user = User::create([
+            'name' => 'Anderies',
+            'email' => 'anderies@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        // Create related profile
+        $user->profile()->create([
+            'phone' => '08123456789',
+            'bio' => 'Lecturer amd tech enthusiast.',
+        ]);
+    }
+}
